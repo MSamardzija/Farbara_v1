d844bb7a440ed355c28a9f3d9b16a88c95befda6
WarCod3x <marko1716@its.edu.rs>
2020-11-19 14:40:17 +0100

    New C_Form, about us and Boje

---
+++ neki.php
@@ -0,0 +1,21 @@
+<?php
+    $name = $_POST['txtName'];
+    $visitor_email = $_POST['txtEmail'];
+    $phone = $_POST['txtPhone'];
+    $message = $_POST['txtMsg'];
+
+    $email_form = 'Eas@gmail.com';
+
+    $email_subject = 'Poruka sa Web Sajta';
+    
+    $email_body = "Poruku salje: $name.\n".
+                    "Mail posiljaoca: $visitor_email.\n". 
+                    "Kontakt telefon: $phone.\n". 
+                    "Poruka: $message.\n";
+    $to = "maresamardzija@gmail.com"
+    $headers = "From: $email_from \r\n";
+    $headers .= "Replay-To: $visitor_email \r\n";
+    
+    mail($to, $email_subject, $email_body, $headers);
+    header("location: index.html");
+?>
