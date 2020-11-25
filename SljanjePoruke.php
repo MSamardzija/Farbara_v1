8628339607f6fbb11fdfc3f1b34319ef51789f5b
WarCod3x <marko1716@its.edu.rs>
2020-11-25 13:19:24 +0100

    V1.01

--- SljanjePoruke.php
+++ SljanjePoruke.php
@@ -1,10 +1,11 @@
 <?php
+
     $name = $_POST['txtName'];
     $visitor_email = $_POST['txtEmail'];
     $phone = $_POST['txtPhone'];
     $message = $_POST['txtMsg'];
 
-    $email_form = 'example@gmail.com';
+    $email_form = 'nope@gmail.com';
 
     $email_subject = 'Poruka sa Web Sajta';
     
@@ -12,10 +13,11 @@
                     "Mail posiljaoca: $visitor_email.\n". 
                     "Kontakt telefon: $phone.\n". 
                     "Poruka: $message.\n";
-    $to = "maresamardzija@gmail.com"
+    $to = "nope@gmail.com"
     $headers = "From: $email_from \r\n";
     $headers .= "Replay-To: $visitor_email \r\n";
+    // Nije jos spremno
     
     mail($to, $email_subject, $email_body, $headers);
-    header("location: index.html");
+    header("location: /index.html");
 ?>
